79a16c6ea1c82d56d9ef79b313fdbc0f7b3807cb
Ghosh <joydeep.d.ghosh@capgemini.com>
2016-07-14 16:35:18 +0530

    project created by participant

---
+++ BankingApplication/src/Client.java
@@ -0,0 +1,24 @@
+
+public class Client {
+
+	public static void main(String[] args) {
+		ICICIBank bank=new ICICIBank();
+		
+		System.out.println(bank.createAccount(101, 3000));
+		
+		System.out.println(bank.createAccount(102, 3000));
+		
+		try
+		{
+			System.out.println("Balance = "+bank.withdrawAmount(101, 2000));
+		}catch(InvalidAccountNumberException ian)
+		{
+			System.out.println("invalid account number");
+		}catch(InsufficientBalanceException ibe)
+		{
+			System.out.println("insufficient balance");
+		}
+
+	}
+
+}
---
+++ BankingApplication/src/ICICIBank.java
@@ -0,0 +1,43 @@
+import java.util.LinkedList;
+
+public class ICICIBank {
+	
+	LinkedList<Account> accounts =new LinkedList<Account>();
+	
+	
+	public String createAccount(int accountNumber,int amount)
+	{
+		
+		Account account=new Account(accountNumber,amount);
+		accounts.add(account);
+		return "account created successfully";
+	}
+	
+	private Account searchAccount(int accountNumber)throws InvalidAccountNumberException
+	{
+		for(Account account:accounts)
+		{
+			if(account.getAccountNumber()==accountNumber)
+			{
+				return account;
+			}
+		}
+		
+		throw new InvalidAccountNumberException();
+	}
+	
+	public int withdrawAmount(int accountNumber,int amount) throws InsufficientBalanceException, InvalidAccountNumberException
+	{
+		Account account=searchAccount(accountNumber);
+		
+		if((account.getAmount()-amount)>=0)
+		{
+			account.setAmount(account.getAmount()-amount);
+			return account.getAmount();
+		}
+		
+		throw new InsufficientBalanceException();
+		
+	}
+
+}
